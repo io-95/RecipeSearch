cdaae8a9f423a46e50a6614115bfd1a3111c5dc4
io-95 <77495768+io-95@users.noreply.github.com>
2025-09-27 23:16:11 +0200

    Migrate project from vue2 to vue3

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
 
-Vue.config.productionTip = false
+const app = createApp(App)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+app.use(router)
+app.use(createPinia())
+
+app.mount('#app')
