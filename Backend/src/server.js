9a81b5205575ffce57fcca25c6fbbe351a166497
io-95 <77495768+io-95@users.noreply.github.com>
2025-09-29 20:59:54 +0200

    Server terminates if port is already in use

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,14 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server läuft auf Port ${PORT}`);
+});
+
+server.on("error", (err) => {
+  if (err.code === "EADDRINUSE") {
+    console.error(`Port ${PORT} is already in use.`);
+    console.error("server error:", err);
+    process.exit(1);
+  }
 });
