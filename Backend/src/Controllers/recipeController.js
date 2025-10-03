19a96227d0894848d8fb662af7ed7add77d28317
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-03 16:20:56 +0200

    Handle statuscode 404 in sendRecipeList, if recipe can't be found

--- Backend/src/Controllers/recipeController.js
+++ Backend/src/Controllers/recipeController.js
@@ -3,9 +3,13 @@ const { getRecipeOfTheDay } = require("../Services/recipeService");
 
 function sendRecipeList(req, res) {
     const keyword = req.querry.search;
-    const recipeList = getRecipeList(keyword);
-
-    res.status(200).json(recipeList);
+    try {
+        const recipeList = getRecipeList(keyword);
+        if(recipeList === null){
+            res.status(404).json("status: Recipe not found");
+        }
+        res.status(200).json(recipeList);
+    }
 }
 
 function sendRecipeOfTheDay(req, res) {
