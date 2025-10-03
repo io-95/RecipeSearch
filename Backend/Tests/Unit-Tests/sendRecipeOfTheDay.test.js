a793d6f04966a4284a910f9d3702269ac7c05e5a
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-03 18:13:42 +0200

    Mock getRecipeOfTheDay and define describe method

--- Backend/Tests/Unit-Tests/sendRecipeOfTheDay.test.js
+++ Backend/Tests/Unit-Tests/sendRecipeOfTheDay.test.js
@@ -1,1 +1,11 @@
 const { sendRecipeOfTheDay } = require("../src/Controllers/recipeController");
+
+jest.mock("../src/Services/recipeService", () => ({
+    getRecipeOfTheDay: jest.fn(),
+}));
+
+const { getRecipeOfTheDay } = require("../src/Service/recipeService");
+
+describe("sendRecipeOfTheDay (Unit-Test)", () => {
+    
+});
