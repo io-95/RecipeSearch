64b098e2b92410bb33726d63fa03360a2f85ef24
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-03 16:24:09 +0200

    Remove try block and send statuscode in sendRecipeList as json

--- Backend/src/Controllers/recipeController.js
+++ Backend/src/Controllers/recipeController.js
@@ -3,13 +3,12 @@ const { getRecipeOfTheDay } = require("../Services/recipeService");
 
 function sendRecipeList(req, res) {
     const keyword = req.querry.search;
-    try {
-        const recipeList = getRecipeList(keyword);
-        if(recipeList === null){
-            res.status(404).json("status: Recipe not found");
-        }
-        res.status(200).json(recipeList);
+    const recipeList = getRecipeList(keyword);
+    
+    if(recipeList === null){
+        res.status(404).json({status: "Recipe not found"});
     }
+    res.status(200).json(recipeList);
 }
 
 function sendRecipeOfTheDay(req, res) {
