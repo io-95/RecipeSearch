4c276df69eeec72356a103e50792d98ce005d5f6
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-04 13:12:29 +0200

    import getRecipeList

--- Backend/Tests/Unit-Tests/sendRecipeList.test.js
+++ Backend/Tests/Unit-Tests/sendRecipeList.test.js
@@ -3,3 +3,6 @@ const { sendRecipeList } = require("../../src/Controllers/recipeController");
 jest.mock("../../src/Controllers/recipeController", () => {
     getRecipeList: jest.fn(),
 });
+
+const { getRecipeList } = require("../../src/Controllers/recipeController");
+
