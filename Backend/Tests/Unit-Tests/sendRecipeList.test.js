e303265d5172c74a796d249d45b75c586f8dc6a9
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-04 13:17:47 +0200

    add query and search to req

--- Backend/Tests/Unit-Tests/sendRecipeList.test.js
+++ Backend/Tests/Unit-Tests/sendRecipeList.test.js
@@ -8,12 +8,21 @@ const { getRecipeList } = require("../../src/Controllers/recipeController");
 
 describe("sendRecipeList (Unit-Test)", () => {
     let req, res;
+    let searchInput;
 
     beforeEach( () => {
-        req = {};
+        req = {
+            query: {
+                search: searchInput
+            }
+        };
         res = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn(),
         };
+    });
+
+    it("return 200 and a list of recipes", () => {
+
     });
 });
