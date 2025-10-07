02c29504555e92fa411a069e77eaf0441dbee918
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-07 20:19:21 +0200

    Implement function to map recipe, extract all ingredients and measures in recipe from DB

---
+++ Backend/src/Model/recipe.mapper.js
@@ -0,0 +1,30 @@
+function mapMealDbRecipe(apiRecipe) {
+    return {
+      id: apiRecipe.idMeal,
+      mealName: apiRecipe.strMeal,
+      instructions: apiRecipe.strInstructions,
+      ingredients: extractIngredients(apiRecipe),
+      measure: extractMeasures(apiRecipe),
+      thumbnail: apiRecipe.strMealThumb
+    };
+  }
+  
+  function extractIngredients(apiRecipe) {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      const name = apiRecipe[`strIngredient${i}`];
+      if (name && name.trim()) ingredients.push({ name });
+    }
+    return ingredients;
+  }
+
+  function extractMeasures(apiRecipe) {
+    const measures = [];
+    for (let i = 1; i <= 20; i++) {
+      const measure = apiRecipe[`strMeasure${i}`];
+      if (measure && measure.trim()) measures.push({ measure });
+    }
+    return measures;
+  }
+
+module.exports = { mapMealDbRecipe }
