7c4cc492ae3f569537ce0e600bfc9747b94275af
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-07 21:47:43 +0200

    delete console logs in mapMealDbRecipe test

--- Backend/Tests/Unit-Tests/mapMealDbRecipe.test.js
+++ Backend/Tests/Unit-Tests/mapMealDbRecipe.test.js
@@ -3,8 +3,7 @@ const { mapMealDbRecipe } = require("../../src/Model/recipe.mapper");
 describe("mapMealDbRecipe (Unit-Test)", () => {
     it("returns a mapped recipe", () => {
         const mappedRecipe = mapMealDbRecipe(mealDbRecipe.meals[0]);
-        //console.log(mappedRecipe);
-        //console.log(recipeOfTheDay.recipe[0]);
+
         expect(mappedRecipe).toEqual(recipeOfTheDay.recipe[0]);
     });
 });
