3945d3ce8301cb69ca89b2171a0ae9c3580862b8
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-09 17:06:27 +0200

    Define cache to save recipe of the day

--- Backend/src/Services/recipeOfTheDayService.js
+++ Backend/src/Services/recipeOfTheDayService.js
@@ -1,3 +1,8 @@
 const axios = require("axios");
 const { mapMealDbRecipe } = require("../models/recipe.mapper.js");
 const { recipeSchema } = require("../models/recipe.schema.js");
+
+let recipeOfTheDayCache = {
+    data: null,
+    lastUpdated: null
+  };
