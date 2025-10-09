6552310b1ed5caa7de0e97a7bc13f7ea61415f72
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-09 17:45:49 +0200

    correct import mistake in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = require("./Routers");
-const { initRecipeScheduler } = require("Services/recipeOfTheDayService");
+const { initRecipeScheduler } = require("./Services/recipeOfTheDayService");
 
 const app = express();
 app.use(express.json());
