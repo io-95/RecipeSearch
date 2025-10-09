bf49006870567b96adedcc3beb9f6abac27166a5
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-09 22:17:35 +0200

    implement getRecipeList in recipeService

--- Backend/src/Services/recipeService.js
+++ Backend/src/Services/recipeService.js
@@ -1,11 +1,32 @@
 const axios = require("axios");
+const { mapMealDbRecipe } = require("../Model/recipe.mapper.js");
+const { recipeModel } = require("../Model/recipe.model.js");
 
 async function getRecipeList(keyword) {
     const recipeList = [];
-    
-    const { data } = await axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + keyword);
-    
-    //throw new Error("DB error");
+    try{
+        const { data } = await axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + keyword);
+        data.meals.forEach((recipe) => {
+            const mapped = mapMealDbRecipe(recipe);
+
+            const { error, value } = recipeModel.validate(mapped);
+            if (!error) {
+                recipeList.push(value);
+            }else {
+                throw new Error(`Invalid recipe data: ${error.message}`);
+            }
+        });
+
+        return {recipe: recipeList}
+    } catch (err) {
+        if (err.response) {
+            throw new Error(`API request failed: ${err.response.status} ${err.response.statusText}`);
+        } else if (err.request) {
+            throw new Error("DB error");
+        } else {
+            throw new Error(`Request setup failed: ${err.message}`);
+        }
+    }
 }
 
 module.exports = { getRecipeList }
