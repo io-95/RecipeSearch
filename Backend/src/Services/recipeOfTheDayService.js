1b84f0ddf4736f2a75a7d8eddd5f0bb23b2689b1
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-09 17:10:23 +0200

    implement fetchRandomRecipe function

--- Backend/src/Services/recipeOfTheDayService.js
+++ Backend/src/Services/recipeOfTheDayService.js
@@ -6,3 +6,17 @@ let recipeOfTheDayCache = {
     data: null,
     lastUpdated: null
   };
+
+  async function fetchRandomRecipe() {
+    const { data } = await axios.get("https://www.themealdb.com/api/json/v1/1/random.php");
+    const rawRecipe = data.meals[0];
+    const mapped = mapMealDbRecipe(rawRecipe);
+  
+    // Validate
+    const { error, value } = recipeSchema.validate(mapped);
+    if (error) {
+      throw new Error(`Invalid recipe data: ${error.message}`);
+    }
+    
+  return value;
+}
