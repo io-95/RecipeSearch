00749a00ffa682905be963540ee888cfb07f28c9
io-95 <77495768+io-95@users.noreply.github.com>
2025-10-09 17:38:23 +0200

    import initRecipeScheduler in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("./Routers");
+cost { intitRecipeScheduler } = require("Service/recipeOfTheDayService");
 
 const app = express();
 app.use(express.json());
