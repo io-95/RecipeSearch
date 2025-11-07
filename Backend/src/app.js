a06913cf80a149a604306cabfdf94e1af3c529e1
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-07 23:05:31 +0100

    use cors in backend, so the browser allows the communication to the backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./Routers");
 const { initRecipeScheduler } = require("./Services/recipeOfTheDayService");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", router);
 
