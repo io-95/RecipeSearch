2ffcccc7fa0196ecb5534d049c39c25d4f29b62b
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-07 22:54:58 +0100

    implement getHealth method in recipeService and call it in app.vue, to see if the backend server is availible

--- Frontend/src/services/recipeService.js
+++ Frontend/src/services/recipeService.js
@@ -1,4 +1,9 @@
-import api from "/api"
+import api from "./api"
+
+export async function getHealth() {
+    const healthStatus = await api.get('/health')
+    console.log(healthStatus)
+}
 
 export async function searchRecipes(querry) {
     
