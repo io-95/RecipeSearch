1ec0aa267312d4ce2f92839529296064bd15db55
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-08 22:52:18 +0100

    change name attribute of landing page component, change background color

---
+++ Frontend/src/views/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="landing_page">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LandingPage",
+    }
+</script>
+
+<style>
+    .landing_page{
+        background-color: brown;
+    }
+</style>
