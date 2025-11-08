a4755c90c1e7d1148dd18e4af7675380fdfc60d3
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-08 21:19:13 +0100

    add a theme object in main.js createVuetify

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -12,6 +12,17 @@ import '@mdi/font/css/materialdesignicons.css'
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#ffeab2',
+          secondary: '#fff6db',
+        },
+      },
+    },
+  },
 })
 
 const app = createApp(App)
