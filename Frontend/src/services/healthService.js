50bfde184e281afc189c765143c453eb4ba91a7d
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-08 14:28:24 +0100

    create healthService move getHealth from recipeService to healthService for better semantic

---
+++ Frontend/src/services/healthService.js
@@ -0,0 +1,6 @@
+import api, { safeApiCall } from "./api"
+
+export async function getHealth() {
+    const healthStatus = await safeApiCall(() => api.get('/health'), { fullResponse: true });
+    console.log(healthStatus.status);
+}
--- Frontend/src/services/recipeService.js
+++ Frontend/src/services/recipeService.js
@@ -1,10 +1,5 @@
 import api, { safeApiCall } from "./api"
 
-export async function getHealth() {
-    const healthStatus = await safeApiCall(() => api.get('/health'), { fullResponse: true });
-    console.log(healthStatus.status);
-}
-
 export async function searchRecipes(querry) {
     
 }
