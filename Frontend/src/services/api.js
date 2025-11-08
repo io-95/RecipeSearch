9112349116d368733f89da70adb39c12132c4258
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-08 13:38:52 +0100

    create a apiError class to handle errors

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,5 +1,14 @@
 import axios from "axios"
 
+export class ApiError extends Error {
+  constructor({ status, message, data }) {
+    super(message);
+    this.name = "ApiError";
+    this.status = status;
+    this.data = data;
+  }
+}
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 5000,
