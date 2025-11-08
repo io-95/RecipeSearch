85b34e7118d824f3369e2faa2d7c26705e7228a6
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-08 22:48:59 +0100

    remove gray hover effect and change hover color of navigationButton

--- Frontend/src/components/NavigationButton.vue
+++ Frontend/src/components/NavigationButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-btn rounded="xl" :color="color" height="40" width=auto>
+    <v-btn class="navButton" rounded="xl" :color="color" height="40" width=auto>
         <slot />
     </v-btn>
 </template>
@@ -15,3 +15,10 @@
         }
     }
 </script>
+
+<style>
+    .navButton:hover{
+        --v-btn-overlay-color: transparent;
+        background-color: #ffc311 !important;
+    }
+</style>
