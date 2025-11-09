826da9d04672c1e79849ecf08754cc231b7c12c0
io-95 <77495768+io-95@users.noreply.github.com>
2025-11-09 15:26:11 +0100

    implement scelleton of searchBar component

---
+++ Frontend/src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+
+export default {
+    name: "SearchBar",
+    data() {
+        return {
+            
+        };
+    },
+    methods: {  
+    },
+    mounted(){     
+    }
+};
+</script>
+
+<style>
+</style>
